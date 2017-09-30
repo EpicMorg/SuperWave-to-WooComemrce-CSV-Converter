11eb697a7a29e627fc4fe94bed7d791a76b4c5aa
STAM <stam@epicm.org>
2017-09-30 15:19:43 +0300

    moved from private repo

---
+++ src/sw2wc/FrmMain.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace oc_2_wp_csv_converter {
+    partial class FrmMain {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.checkBoxStatus = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelSelectedCat = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox);
+            this.groupBox1.Controls.Add(this.checkBoxStatus);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 210);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Шаг 1. Оригинальная CSV";
+            this.toolTip.SetToolTip(this.groupBox1, "Дропнуть CSV-шку поставщика. Ожидается, что будет CSV-шка определенной категории." +
+        " All-in-one тут точно не нужен!");
+            // 
+            // listBox
+            // 
+            this.listBox.AllowDrop = true;
+            this.listBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(6, 19);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(247, 160);
+            this.listBox.TabIndex = 1;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            this.listBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox_DragDrop);
+            this.listBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox_DragEnter);
+            // 
+            // checkBoxStatus
+            // 
+            this.checkBoxStatus.AutoSize = true;
+            this.checkBoxStatus.Enabled = false;
+            this.checkBoxStatus.Location = new System.Drawing.Point(169, 187);
+            this.checkBoxStatus.Name = "checkBoxStatus";
+            this.checkBoxStatus.Size = new System.Drawing.Size(84, 17);
+            this.checkBoxStatus.TabIndex = 0;
+            this.checkBoxStatus.Text = "Готовность";
+            this.checkBoxStatus.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelSelectedCat);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.comboBoxCategory);
+            this.groupBox2.Location = new System.Drawing.Point(277, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(367, 126);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Шаг 2. Выбор категории";
+            this.toolTip.SetToolTip(this.groupBox2, "Для всех товаров в выбранной CSV-шке будет применена новая выбраная категория из " +
+        "списка.");
+            // 
+            // labelSelectedCat
+            // 
+            this.labelSelectedCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSelectedCat.Location = new System.Drawing.Point(6, 56);
+            this.labelSelectedCat.Name = "labelSelectedCat";
+            this.labelSelectedCat.Size = new System.Drawing.Size(355, 67);
+            this.labelSelectedCat.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выбрано:";
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Items.AddRange(new object[] {
+            "GMO > Запасные части Xerox GMO",
+            "GMO > Оборудование Xerox GMO > Конвертовальные системы Xerox",
+            "GMO > Оборудование Xerox GMO > Офисное оборудование Xerox монохромное",
+            "GMO > Оборудование Xerox GMO > Офисное оборудование Xerox цветное",
+            "GMO > Оборудование Xerox GMO > Программное обеспечение Xerox",
+            "GMO > Оборудование Xerox GMO > Широкоформатное оборудование Xerox",
+            "GMO > Расходные материалы Xerox GMO",
+            "HVD > Запасные части Xerox HVD",
+            "HVD > Оборудование Xerox HVD > Мониторы и сканеры Xerox",
+            "HVD > Оборудование Xerox HVD > МФУ Xerox A3 ",
+            "HVD > Оборудование Xerox HVD > МФУ Xerox A4 Color",
+            "HVD > Оборудование Xerox HVD > МФУ Xerox A4 Mono ",
+            "HVD > Оборудование Xerox HVD > Принтеры Xerox Color ",
+            "HVD > Оборудование Xerox HVD > Принтеры Xerox Mono ",
+            "HVD > Расходные материалы Xerox HVD ",
+            "HVD > Расходные материалы XRC"});
+            this.comboBoxCategory.Location = new System.Drawing.Point(6, 19);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(355, 21);
+            this.comboBoxCategory.TabIndex = 0;
+            this.comboBoxCategory.SelectedIndexChanged += new System.EventHandler(this.comboBoxCategory_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Location = new System.Drawing.Point(277, 144);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(367, 78);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Шаг 3. Наценка в %, после конвертации курса в рубли";
+            this.toolTip.SetToolTip(this.groupBox3, "Стандартная наценка - 10% сверху после конвертации в рубли по актуальному курсу.");
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 27);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(355, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.buttonConvert);
+            this.groupBox4.Location = new System.Drawing.Point(12, 228);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(632, 95);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Шаг 4. Экспорт";
+            this.toolTip.SetToolTip(this.groupBox4, "Сделать пиздато (с)");
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(6, 19);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(620, 70);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Поехали";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "CSV|*.csv";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 335);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Superwave to WooCommerce CSV converter, 1:55, 30.09.2017";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.CheckBox checkBoxStatus;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.ComboBox comboBoxCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelSelectedCat;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ src/sw2wc/FrmMain.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace oc_2_wp_csv_converter {
+    public partial class FrmMain : Form
+    {
+      
+        public FrmMain() {
+            InitializeComponent();
+        }
+
+        /* 
+           Это список всех катерогий. Скаждой строки новая. 
+        
+            HVD > Запасные части Xerox HVD
+            HVD > Оборудование Xerox HVD > Мониторы и сканеры Xerox
+            HVD > Оборудование Xerox HVD > МФУ Xerox A3 
+            HVD > Оборудование Xerox HVD > МФУ Xerox A4 Color
+            HVD > Оборудование Xerox HVD > МФУ Xerox A4 Mono 
+            HVD > Оборудование Xerox HVD > Принтеры Xerox Color 
+            HVD > Оборудование Xerox HVD > Принтеры Xerox Mono 
+            HVD > Расходные материалы Xerox HVD 
+            HVD > Расходные материалы XRC 
+            GMO > Запасные части Xerox GMO
+            GMO > Оборудование Xerox GMO > Конвертовальные системы Xerox
+            GMO > Оборудование Xerox GMO > Офисное оборудование Xerox монохромное
+            GMO > Оборудование Xerox GMO > Офисное оборудование Xerox цветное
+            GMO > Оборудование Xerox GMO > Программное обеспечение Xerox
+            GMO > Оборудование Xerox GMO > Широкоформатное оборудование Xerox
+            GMO > Расходные материалы Xerox GMO
+
+            А это формат CSV для экспорта: 
+            Артикул,Имя,"Короткое описание",Описание,"В наличии?","Базовая цена",Категории
+            string,string,string,string,int,int,string
+
+        */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxCategory.SelectedIndex = 0;
+        }
+
+        private void listBox_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true) {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void listBox_DragDrop(object sender, DragEventArgs e) {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files) {
+                string input = null; 
+                using (StreamReader sr = new StreamReader(file, Encoding.GetEncoding(1251)))
+                {
+                    sr.ReadLine();
+                    while((input = sr.ReadLine()) != null)
+                    listBox.Items.Add(input);
+
+                    
+                }
+
+
+               
+            }
+            checkBoxStatus.Checked = true;
+        }
+
+        private void comboBoxCategory_SelectedIndexChanged(object sender, EventArgs e) {
+
+            labelSelectedCat.Text = comboBoxCategory.Text;
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("https://www.cbr-xml-daily.ru/daily_utf8.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            double USD = 0;
+            foreach (XmlNode xn in xRoot.ChildNodes)
+            {
+                if (xn.Attributes[0].Value.Contains("R01235"))
+                {
+                    USD = Double.Parse(xn.ChildNodes[4].InnerText);
+                }
+
+            }
+
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+
+            foreach (var item in listBox.Items)
+            {
+
+                string[] csv = ((string) item).Split(';');
+                //if (csv.Length != 9) continue;
+                for (int i = 0; i < csv.Length; i++)
+                {
+                    csv[i] = csv[i].Trim('"', '=');
+                }
+
+                string price = csv[7];
+
+                if (price.IndexOf("руб") < 0) {
+                    price = price.Trim(' ', '$');
+                    //if (price == "нет") continue; // да какой уёбок будет писать НЕТ в значение инта?! ах, ну да..
+                    double result = 0;
+                    Double.TryParse(price, NumberStyles.None, provider, out result);
+                    price = (result * USD).ToString();
+                } else {
+                    price = price.Replace("руб", "").Trim('.', ' ', '"', '=');
+                }
+
+
+                price = Math.Round(((double)numericUpDown1.Value + 100) * Convert.ToDouble(price) / 100, 0, MidpointRounding.AwayFromZero).ToString();
+
+                // воркэраунд от пидоров, которые ставят цену 0 и количество товара больше 0. суки.
+                if (price == "0") {
+                    csv[3] = "0";
+                }
+
+                sb.AppendLine($"{csv[0]},\"{csv[1]}\",\"{csv[1]}\",\"{csv[1]}\",{csv[3]},{price},\"{comboBoxCategory.Text.Trim(' ')}\"");
+
+            }
+            saveFileDialog.ShowDialog();
+            using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName)) {
+                sb.Insert(0, "Артикул,Имя,\"Короткое описание\",Описание,\"В наличии?\",\"Базовая цена\",\"Категории\"\n");
+                sw.WriteLine(sb.ToString());
+                MessageBox.Show(@"Работа завершена!",@"Готово",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                Application.Restart();
+            }
+
+        }
+    }
+}
